2ff5638f838f5bd30a1c4f765158145307304b19
Brandon Walkin <bwalkin@marketcircle.com>
2009-02-22 19:23:21 -0500

    Added image rotating functionality to NSImage category

--- NSImage+BWAdditions.h
+++ NSImage+BWAdditions.h
@@ -13,4 +13,7 @@
 // Draw a solid color over an image - taking into account alpha. Useful for coloring template images.
 - (NSImage *)tintedImageWithColor:(NSColor *)tint;
 
+// Rotate an image 90 degrees clockwise or counterclockwise
+- (NSImage *)rotateImage90DegreesClockwise:(BOOL)clockwise;
+
 @end
