0e62f61c2afd420e856a3a1f893fb11228c999c2
Brandon Walkin <bwalkin@marketcircle.com>
2009-06-04 00:21:04 -0400

    Added hyperlink button library item

---
+++ BWHyperlinkButton.m
@@ -0,0 +1,54 @@
+//
+//  BWHyperlinkButton.m
+//  BWToolkit
+//
+//  Created by Brandon Walkin (www.brandonwalkin.com)
+//  All code is provided under the New BSD license.
+//
+
+#import "BWHyperlinkButton.h"
+#import "BWHyperlinkButtonCell.h"
+
+@implementation BWHyperlinkButton
+
+@synthesize urlString;
+
++ (Class)cellClass
+{
+	return [BWHyperlinkButtonCell class];
+}
+
+-(void)awakeFromNib
+{
+	[self setTarget:self];
+	[self setAction:@selector(openURLInBrowser:)];
+}
+
+- (id)initWithCoder:(NSCoder *)decoder
+{
+	if ((self = [super initWithCoder:decoder]) != nil)
+	{
+		[self setUrlString:[decoder decodeObjectForKey:@"BWHBUrlString"]];	
+	}
+	return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder
+{
+    [super encodeWithCoder:coder];
+
+	[coder encodeObject:[self urlString] forKey:@"BWHBUrlString"];
+} 
+
+- (void)openURLInBrowser:(id)sender
+{
+	if (![self respondsToSelector:@selector(ibDidAddToDesignableDocument:)])
+		[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:self.urlString]];
+}
+
+- (void)resetCursorRects 
+{
+	[self addCursorRect:[self bounds] cursor:[NSCursor pointingHandCursor]];
+}
+
+@end
