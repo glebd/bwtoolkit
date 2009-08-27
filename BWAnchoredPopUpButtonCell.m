ab84e6c832ae3fbdc8d5cc81fcbdaadd1587124a
Brandon Walkin <bwalkin@marketcircle.com>
2009-08-27 02:27:43 -0400

    Rewrote Anchored Pop Up Button

--- BWAnchoredPopUpButtonCell.m
+++ BWAnchoredPopUpButtonCell.m
@@ -12,20 +12,23 @@
 #import "NSColor+BWAdditions.h"
 #import "NSImage+BWAdditions.h"
 
-#define IMAGE_INSET 8;
-#define ARROW_INSET 11;
-
 static NSColor *fillStop1, *fillStop2, *fillStop3, *fillStop4;
 static NSColor *topBorderColor, *bottomBorderColor, *sideBorderColor, *sideInsetColor, *pressedColor;
-static NSColor *textColor, *textShadowColor, *imageColor, *imageShadowColor;
+static NSColor *enabledTextColor, *disabledTextColor, *enabledImageColor, *disabledImageColor;
 static NSColor *borderedSideBorderColor, *borderedTopBorderColor;
 static NSGradient *fillGradient;
 static NSImage *pullDownArrow;
-static float scaleFactor = 1.0f;
+static NSShadow *contentShadow;
+static float arrowInset = 11.0;
+
+@interface NSCell (BWAPUBCPrivate)
+- (NSDictionary *)_textAttributes;
+@end
 
 @interface BWAnchoredPopUpButtonCell (BWAPUBCPrivate)
-- (void)drawTitleInFrame:(NSRect)cellFrame;
-- (void)drawImageInFrame:(NSRect)cellFrame;
+- (NSColor *)textColor;
+- (NSColor *)imageColor;
+- (NSRect)highlightRectForBounds:(NSRect)cellFrame;
 - (void)drawArrowInFrame:(NSRect)cellFrame;
 @end
 
@@ -52,36 +55,59 @@ static float scaleFactor = 1.0f;
 	
 	pressedColor			= [[NSColor colorWithCalibratedWhite:(0.0f / 255.0f) alpha:0.35] retain];
 	
-	textColor				= [[NSColor colorWithCalibratedWhite:(10.0f / 255.0f) alpha:1] retain];
-	textShadowColor			= [[NSColor colorWithCalibratedWhite:(255.0f / 255.0f) alpha:0.75] retain];
+	enabledTextColor	= [[NSColor colorWithCalibratedWhite:(10.0f / 255.0f) alpha:1] retain];
+	disabledTextColor	= [[enabledTextColor colorWithAlphaComponent:0.6] retain];
 	
-	imageColor				= [[NSColor colorWithCalibratedWhite:(70.0f / 255.0f) alpha:1] retain];
-	imageShadowColor		= [[NSColor colorWithCalibratedWhite:(240.0f / 255.0f) alpha:1] retain];
+	enabledImageColor	= [[NSColor colorWithCalibratedWhite:(72.0f / 255.0f) alpha:1] retain];
+	disabledImageColor	= [[enabledImageColor colorWithAlphaComponent:0.6] retain];
 	
 	borderedSideBorderColor	= [[NSColor colorWithCalibratedWhite:(0.0f / 255.0f) alpha:0.25] retain];
 	borderedTopBorderColor	= [[NSColor colorWithCalibratedWhite:(190.0f / 255.0f) alpha:1] retain];
 	
-	if([BWAnchoredPopUpButtonCell class] == [self class])
+	contentShadow = [[NSShadow alloc] init];
+	[contentShadow setShadowOffset:NSMakeSize(0,-1)];
+	[contentShadow setShadowColor:[NSColor colorWithCalibratedWhite:(255.0f / 255.0f) alpha:0.75]];
+
+	NSBundle *bundle = [NSBundle bundleForClass:[BWAnchoredPopUpButtonCell class]];		
+	pullDownArrow = [[NSImage alloc] initWithContentsOfFile:[bundle pathForImageResource:@"ButtonBarPullDownArrow.pdf"]];
+}
+
+- (NSControlSize)controlSize
+{
+	return NSSmallControlSize;
+}
+
+- (void)setControlSize:(NSControlSize)size
+{
+	
+}
+
+#pragma mark Draw Bezel & Arrows
+
+- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
+{
+	[super drawWithFrame:cellFrame inView:controlView];
+
+	if ([self isHighlighted])
 	{
-		NSBundle *bundle = [NSBundle bundleForClass:[BWAnchoredPopUpButtonCell class]];
-		
-		pullDownArrow = [[[NSImage alloc] initWithContentsOfFile:[bundle pathForImageResource:@"ButtonBarPullDownArrow.pdf"]] retain];
+		[pressedColor set];
+		NSRectFillUsingOperation([self highlightRectForBounds:cellFrame], NSCompositeSourceOver);
 	}
 }
 
-- (void)awakeFromNib
+- (NSRect)highlightRectForBounds:(NSRect)bounds
 {
-	scaleFactor = [[NSScreen mainScreen] userSpaceScaleFactor];
+	return bounds;
 }
 
-- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
-{		
+- (void)drawBorderAndBackgroundWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
+{
 	BOOL inBorderedBar = YES;
 	
 	if ([[[self controlView] superview] respondsToSelector:@selector(isAtBottom)])
 	{
 		if ([(BWAnchoredButtonBar *)[[self controlView] superview] isAtBottom])
-			inBorderedBar = NO;
+			inBorderedBar = NO;			
 	}
 	
 	[fillGradient drawInRect:cellFrame angle:90];
@@ -102,7 +128,7 @@ static float scaleFactor = 1.0f;
 		[sideBorderColor bwDrawPixelThickLineAtPosition:0 withInset:1 inRect:cellFrame inView:[self controlView] horizontal:NO flip:NO];
 		[sideBorderColor bwDrawPixelThickLineAtPosition:0 withInset:1 inRect:cellFrame inView:[self controlView] horizontal:NO flip:YES];
 	}
-	
+
 	if (inBorderedBar && [[self controlView] respondsToSelector:@selector(isAtLeftEdgeOfBar)])
 	{
 		if ([(BWAnchoredPopUpButton *)[self controlView] isAtLeftEdgeOfBar])
@@ -111,161 +137,124 @@ static float scaleFactor = 1.0f;
 			[bottomBorderColor bwDrawPixelThickLineAtPosition:0 withInset:1 inRect:cellFrame inView:[self controlView] horizontal:NO flip:YES];
 	}
 	
-	if ([self image] == nil)
-		[self drawTitleInFrame:cellFrame];
-	else
-		[self drawImageInFrame:cellFrame];
-	
 	[self drawArrowInFrame:cellFrame];
-	
-	if ([self isHighlighted])
-	{
-		[pressedColor set];
-		NSRectFillUsingOperation(cellFrame, NSCompositeSourceOver);
-	}
 }
 
-- (void)drawTitleInFrame:(NSRect)cellFrame
+- (void)drawArrowInFrame:(NSRect)cellFrame
 {
-	if (![[self title] isEqualToString:@""])
-	{
-		NSColor *localTextColor = textColor;
-		
-		if (![self isEnabled])
-		{
-			localTextColor = [textColor colorWithAlphaComponent:0.6];
-		}
-		
-		NSMutableDictionary *attributes = [[[NSMutableDictionary alloc] init] autorelease];
-		[attributes addEntriesFromDictionary:[[self attributedTitle] attributesAtIndex:0 effectiveRange:NULL]];
-		[attributes setObject:localTextColor forKey:NSForegroundColorAttributeName];
-		[attributes setObject:[NSFont systemFontOfSize:11] forKey:NSFontAttributeName];
-		
-		NSShadow *shadow = [[[NSShadow alloc] init] autorelease];
-		[shadow setShadowOffset:NSMakeSize(0,-1)];
-		[shadow setShadowColor:textShadowColor];
-		[attributes setObject:shadow forKey:NSShadowAttributeName];
-		
-		NSMutableAttributedString *string = [[[NSMutableAttributedString alloc] initWithString:[self title] attributes:attributes] autorelease];
-		
-		// Draw title
-		NSRect boundingRect = [string boundingRectWithSize:cellFrame.size options:0];
-		
-		NSPoint cellCenter;
-		cellCenter.y = cellFrame.size.height / 2;
-		
-		NSPoint drawPoint = cellCenter;
-		drawPoint.y -= boundingRect.size.height / 2;
-		drawPoint.y = roundf(drawPoint.y);
-		
-		drawPoint.x = IMAGE_INSET;
+	NSPoint drawPoint;
+	drawPoint.x = NSMaxX(cellFrame) - arrowInset;
 	
-		[string drawAtPoint:drawPoint];
+	NSImage *arrow = [pullDownArrow bwTintedImageWithColor:[self imageColor]];
+	
+	NSAffineTransform* transform = [NSAffineTransform transform];
+	[transform translateXBy:0.0 yBy:cellFrame.size.height];
+	[transform scaleXBy:1.0 yBy:-1.0];
+	[transform concat];
+	
+	[contentShadow set];
+	
+	if ([self pullsDown])
+	{
+		// Draw pull-down arrow
+		drawPoint.y = roundf(cellFrame.size.height / 2) - 2;
+		[arrow drawAtPoint:drawPoint fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
 	}
+	else
+	{
+		// Draw bottom pop-up arrow
+		drawPoint.y = roundf(cellFrame.size.height / 2) - 4;
+		[arrow drawAtPoint:drawPoint fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
+		
+		// Draw top pop-up arrow
+		drawPoint.y -= 1;
+		
+		NSAffineTransform* transform2 = [NSAffineTransform transform];
+		[transform2 translateXBy:0.0 yBy:cellFrame.size.height];
+		[transform2 scaleXBy:1.0 yBy:-1.0];
+		[transform2 concat];
+		
+		[arrow drawAtPoint:drawPoint fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
+		
+		[transform2 invert];
+		[transform2 concat];
+	}
+
+	[transform invert];
+	[transform concat];	
 }
 
-- (void)drawImageInFrame:(NSRect)cellFrame
+#pragma mark Draw Title
+
+- (NSColor *)textColor
 {
+	return [self isEnabled] ? enabledTextColor : disabledTextColor;
+}
+
+- (NSDictionary *)_textAttributes
+{
+	NSMutableDictionary *attributes = [[[NSMutableDictionary alloc] init] autorelease];
+	[attributes addEntriesFromDictionary:[super _textAttributes]];
+	[attributes setObject:[self textColor] forKey:NSForegroundColorAttributeName];
+	[attributes setObject:[NSFont systemFontOfSize:11] forKey:NSFontAttributeName];
+	[attributes setObject:contentShadow forKey:NSShadowAttributeName];
+	
+	return attributes;
+}
+
+- (NSRect)titleRectForBounds:(NSRect)bounds
+{
+	NSRect titleRect = [super titleRectForBounds:bounds];
+	
+	titleRect.origin.y -= 1;
+	titleRect = NSInsetRect(titleRect, 3, 0);
+	
+	return titleRect;
+}
+
+#pragma mark Draw Image
+
+- (NSColor *)imageColor
+{
+	return [self isEnabled] ? enabledImageColor : disabledImageColor;
+}
+
+- (NSRect)imageRectForBounds:(NSRect)theRect
+{
+	NSRect imageRect = [super imageRectForBounds:theRect];
+	
+	if (imageRect.size.width < [self image].size.width)
+		imageRect.size.width += 1;
+	
+	imageRect.origin.x += 1;
+	
+	return imageRect;
+}
+
+- (void)drawImageWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
+{	
 	NSImage *image = [self image];
 	
 	if (image != nil)
 	{
-		[image setScalesWhenResized:NO];
-		NSRect sourceRect = NSZeroRect;
-		
 		if ([[image name] isEqualToString:@"NSActionTemplate"])
 			[image setSize:NSMakeSize(10,10)];
 		
-		sourceRect.size = [image size];
-		
-		NSPoint backgroundCenter;
-		backgroundCenter.y = cellFrame.size.height / 2;
-		
-		NSPoint drawPoint = backgroundCenter;
-		drawPoint.y -= sourceRect.size.height / 2 ;//+ 0.5;
-		drawPoint.y = roundf(drawPoint.y);
-		
-		drawPoint.x = IMAGE_INSET;
-		
-		NSAffineTransform* transform = [NSAffineTransform transform];
-		[transform translateXBy:0.0 yBy:cellFrame.size.height];
-		[transform scaleXBy:1.0 yBy:-1.0];
-		[transform concat];
+		NSImage *newImage = image;
 		
 		if ([image isTemplate])
-		{
-			NSImage *glyphImage = [image bwTintedImageWithColor:imageColor];
-			NSImage *shadowImage = [image bwTintedImageWithColor:imageShadowColor];
-			NSPoint shadowPoint = drawPoint;
-			shadowPoint.y--;
-			
-			[shadowImage drawAtPoint:shadowPoint fromRect:sourceRect operation:NSCompositeSourceOver fraction:1];		
-			
-			if ([self isEnabled])
-				[glyphImage drawAtPoint:drawPoint fromRect:sourceRect operation:NSCompositeSourceOver fraction:1];
-			else
-				[glyphImage	drawAtPoint:drawPoint fromRect:sourceRect operation:NSCompositeSourceOver fraction:0.5];
-			
-			[transform invert];
-			[transform concat];
-		}
-		else
-		{
-			if ([self isEnabled])
-				[image drawAtPoint:drawPoint fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1];
-			else
-				[image drawAtPoint:drawPoint fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:0.5];
-			
-			// Run the flip transform again so the arrow doesn't draw upside-down
-			NSAffineTransform* transform = [NSAffineTransform transform];
-			[transform translateXBy:0.0 yBy:cellFrame.size.height];
-			[transform scaleXBy:1.0 yBy:-1.0];
-			[transform concat];
-		}
-	}
-}
-- (void)drawArrowInFrame:(NSRect)cellFrame
-{
-	if ([self pullsDown])
-	{
-		NSPoint drawPoint;
-		drawPoint.x = NSMaxX(cellFrame) - ARROW_INSET;
-		drawPoint.y = roundf(cellFrame.size.height / 2) - 2;
+			newImage = [image bwTintedImageWithColor:[self imageColor]];
 
-		NSImage *glyphImage = [pullDownArrow bwTintedImageWithColor:imageColor];
-		NSImage *shadowImage = [pullDownArrow bwTintedImageWithColor:imageShadowColor];
-		NSPoint shadowPoint = drawPoint;
-		shadowPoint.y--;
-		
 		NSAffineTransform* transform = [NSAffineTransform transform];
 		[transform translateXBy:0.0 yBy:cellFrame.size.height];
 		[transform scaleXBy:1.0 yBy:-1.0];
 		[transform concat];
 		
-		[shadowImage drawAtPoint:shadowPoint fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1];
-		
-		if ([self isEnabled])
-			[glyphImage drawAtPoint:drawPoint fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1];
-		else
-			[glyphImage drawAtPoint:drawPoint fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:0.5];
-		
+		[newImage drawInRect:[self imageRectForBounds:cellFrame] fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1];
+
 		[transform invert];
 		[transform concat];
-	}
-	else
-	{
-		// Doesn't support pop-up style yet
-	}
-}
-
-- (NSControlSize)controlSize
-{
-	return NSSmallControlSize;
-}
-
-- (void)setControlSize:(NSControlSize)size
-{
-	
+	}	
 }
 
 @end
