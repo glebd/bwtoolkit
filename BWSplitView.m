5157a3da93cd1377c2a5bc56915461affc3dc138
Brandon Walkin <bwalkin@marketcircle.com>
2009-11-05 04:02:06 -0500

    Split view - Added support for thick vertical dividers

--- BWSplitView.m
+++ BWSplitView.m
@@ -119,30 +119,16 @@ static float scaleFactor = 1.0f;
 
 - (void)drawDividerInRect:(NSRect)aRect
 {	
-    if ([self isVertical])
-    {
-		aRect.size.width = [self dividerThickness];
-		
+	if ([self dividerThickness] < 1.01)
+	{
 		if (colorIsEnabled && color != nil)
 			[color drawSwatchInRect:aRect];
 		else
 			[super drawDividerInRect:aRect];
-    }
+	}
 	else
 	{
-		aRect.size.height = [self dividerThickness];
-		
-		if ([self dividerThickness] <= 1.01)
-		{
-			if (colorIsEnabled && color != nil)
-				[color drawSwatchInRect:aRect];
-			else
-				[super drawDividerInRect:aRect];
-		}
-		else
-		{
-			[self drawGradientDividerInRect:aRect];
-		}
+		[self drawGradientDividerInRect:aRect];
 	}
 }
 
@@ -150,13 +136,26 @@ static float scaleFactor = 1.0f;
 {	
 	aRect = [self centerScanRect:aRect];
 
-	// Draw gradient
-	NSRect gradRect = NSMakeRect(aRect.origin.x,aRect.origin.y + 1 / scaleFactor,aRect.size.width,aRect.size.height - 1 / scaleFactor);
-	[gradient drawInRect:gradRect angle:90];
+	if ([self isVertical])
+	{
+		// Draw gradient
+		NSRect gradRect = NSMakeRect(aRect.origin.x + 1 / scaleFactor,aRect.origin.y,aRect.size.width - 1 / scaleFactor,aRect.size.height);
+		[gradient drawInRect:gradRect angle:0];
 	
-	// Draw top and bottom borders
-	[borderColor bwDrawPixelThickLineAtPosition:0 withInset:0 inRect:aRect inView:self horizontal:YES flip:NO];
-	[borderColor bwDrawPixelThickLineAtPosition:0 withInset:0 inRect:aRect inView:self horizontal:YES flip:YES];
+		// Draw left and right borders
+		[borderColor bwDrawPixelThickLineAtPosition:0 withInset:0 inRect:aRect inView:self horizontal:NO flip:NO];
+		[borderColor bwDrawPixelThickLineAtPosition:0 withInset:0 inRect:aRect inView:self horizontal:NO flip:YES];	
+	}
+	else
+	{
+		// Draw gradient
+		NSRect gradRect = NSMakeRect(aRect.origin.x,aRect.origin.y + 1 / scaleFactor,aRect.size.width,aRect.size.height - 1 / scaleFactor);
+		[gradient drawInRect:gradRect angle:90];
+	
+		// Draw top and bottom borders
+		[borderColor bwDrawPixelThickLineAtPosition:0 withInset:0 inRect:aRect inView:self horizontal:YES flip:NO];
+		[borderColor bwDrawPixelThickLineAtPosition:0 withInset:0 inRect:aRect inView:self horizontal:YES flip:YES];		
+	}
 	
 	[self drawDimpleInRect:aRect];
 }
@@ -188,25 +187,6 @@ static float scaleFactor = 1.0f;
 		NSRect dimpleRect = NSMakeRect(0,0,[dimpleImageVector size].width,[dimpleImageVector size].height);
 		[dimpleImageVector drawInRect:destRect fromRect:dimpleRect operation:NSCompositeSourceOver fraction:1];
 	}
-}
-
-- (CGFloat)dividerThickness
-{
-	float thickness;
-	
-    if ([self isVertical])
-	{
-		thickness = 1;
-	}
-	else
-	{
-		if ([super dividerThickness] < 1.01)
-			thickness = 1;
-		else
-			thickness = 10;
-	}
-	
-    return thickness;
 }
 
 - (void)setDelegate:(id)anObj
@@ -1360,7 +1340,7 @@ static float scaleFactor = 1.0f;
 	}
 }
 
-#pragma mark Force Vertical Splitters to Thin Appearance
+#pragma mark IB Inspector Support Notifications
 
 // This class doesn't have an appearance for wide vertical splitters, so we force all vertical splitters to thin.
 // We also post notifications that are used by the inspector to show & hide controls.
@@ -1372,10 +1352,7 @@ static float scaleFactor = 1.0f;
 	if (aStyle != [self dividerStyle])
 		styleChanged = YES;
 	
-	if ([self isVertical])
-		[super setDividerStyle:NSSplitViewDividerStyleThin];
-	else
-		[super setDividerStyle:aStyle];
+	[super setDividerStyle:aStyle];
 	
 	// There can be sizing issues during design-time if we don't call this
 	[self adjustSubviews];
@@ -1391,9 +1368,6 @@ static float scaleFactor = 1.0f;
 	if (flag != [self isVertical])
 		orientationChanged = YES;
 		
-	if (flag)
-		[super setDividerStyle:NSSplitViewDividerStyleThin];
-	
 	[super setVertical:flag];
 	
 	if (orientationChanged)
